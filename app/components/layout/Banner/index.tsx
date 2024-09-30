0fd656bb49ad532a30970bc91167bf342712f51a
Marcos Longo <marcosv.longo@gmail.com>
2024-09-30 14:58:38 -0300

    lint

--- app/components/layout/Banner/index.tsx
+++ app/components/layout/Banner/index.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { motion } from "framer-motion";
-import Image from "next/image";
 import { Video } from "./Video";
 import { HighlightSection } from "./Highlight";
 import Link from "next/link";
