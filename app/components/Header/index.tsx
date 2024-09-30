26c0f7328c7a3f3e3afc40cc2810b1d57ae585a1
Marcos Longo <marcosv.longo@gmail.com>
2024-09-30 15:22:46 -0300

    feat: implementa header

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { MdOutlineMailOutline, MdOutlinePhone } from "react-icons/md";
@@ -5,28 +7,43 @@ import { MenuDesktop } from "./MenuDesktop";
 import { MenuMobile } from "./MenuMobile";
 
 export function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <>
-      <header className="border-b border-[#ccc]">
-        <div className="logo-bg-orange">
-          <div className="container flex flex-col gap-2 md:gap-0 md:flex-row justify-between md:items-center py-3 text-white font-semibold md:text-lg">
-            <Link href={'mailto:rederealmais@bateriasreal.com.br'} className="flex items-center gap-2 hover:underline"><MdOutlineMailOutline size={26} /> rederealmais@bateriasreal.com.br</Link>
-            <Link href={''} className="flex items-center gap-2 hover:underline"><MdOutlinePhone size={26} /> (11) 96603-3023</Link>
-          </div>
-        </div>
-        <div className="container flex justify-between items-center py-4">
-          <Link href="">
-            <Image
-              src={'assets/images/realmais.svg'}
-              width={170}
-              height={80}
-              alt="Rede Real+"
-            />
+    <header className={`border-b border-[#ccc] w-full z-50 bg-white transition-all ${isScrolled ? 'fixed' : 'relative'}`}>
+      <div className="logo-bg-orange">
+        <div className={`container flex-col gap-2 md:gap-0 md:flex-row justify-between md:items-center py-3 text-white font-semibold md:text-lg ${isScrolled ? 'hidden' : 'flex'}`}>
+          <Link href="mailto:rederealmais@bateriasreal.com.br" className="flex items-center gap-2 hover:underline">
+            <MdOutlineMailOutline size={26} /> rederealmais@bateriasreal.com.br
           </Link>
-          <MenuDesktop />          
-          <MenuMobile />
-        </div>        
-      </header>
-    </>
+          <Link href="" className="flex items-center gap-2 hover:underline">
+            <MdOutlinePhone size={26} /> (11) 96603-3023
+          </Link>
+        </div>
+      </div>
+      <div className="container flex justify-between items-center py-4">
+        <Link href="">
+          <Image src="assets/images/realmais.svg" width={170} height={80} alt="Rede Real+" />
+        </Link>
+        <MenuDesktop />
+        <MenuMobile />
+      </div>
+    </header>
   );
 }
